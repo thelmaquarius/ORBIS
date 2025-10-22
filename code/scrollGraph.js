ac000b01648a25e7c2bcb26cfacfbdc119ad9563
Youma <zoor@netcourrier.com>
2025-10-22 09:00:45 +0200

    Ajout évolution 20 langues data extrapolées

--- code/scrollGraph.js
+++ code/scrollGraph.js
@@ -1,14 +1,16 @@
 // scrollGraph.js
 export async function initScrollGraph() {
-  const res = await fetch('/data/evolution_20_langues.json');
+  const res = await fetch("/data/evolution_20_langues_interpolated.json");
   const data = await res.json();
-
-  const container = document.getElementById('graph-container');
+  const container = document.getElementById("graph-container");
   if (!data?.length) {
     console.error("Aucun jeu de données trouvé !");
     return;
   }
-  data.forEach(lang => {
-    console.log(lang['1900'])
+  data.forEach((lang) => {
+    console.log("=========LANGUE=========")
+    for (let i = 1900; i < 2025; i++) {
+      console.log(lang[i]);
+    }
   });
-};
+}
